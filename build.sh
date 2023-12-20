04b4a3427c1399ede5ac05d3616de2d12854b5a4
slothy <jacobcalvining@gmail.com>
2023-12-20 15:41:13 -0600

    updated readme and build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
 if ls ./bin > /dev/null 2>&1 ; then 
-gcc compilation.c -o bin/compilation
+gcc example.c -o bin/example
 gcc test.c -o bin/test
 else 
   mkdir ./bin
 fi
 if ls ./examples > /dev/null 2>&1 ; then
-  touch test
+  touch examples/test
 else 
   mkdir ./examples
 fi
